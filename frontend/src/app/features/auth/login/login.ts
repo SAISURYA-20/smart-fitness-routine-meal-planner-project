7afbb649669314eac5070e672bfcda5badb4126c
SAISURYA-20 <suryakaruturi20@gmail.com>
2025-12-16 14:06:07 +0530

    feat(auth): implement Angular Material login UI with validations

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './login.html',
   styleUrls: ['./login.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+    }
+  }
 }
